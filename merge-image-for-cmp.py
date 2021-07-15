628756cd744449361980ad90e7caf9c7bb5f87d1
Jian Feng Ye <yejianfengblue@163.com>
2021-07-15 16:21:49 +0800

    Merge all curve.centercol images into one

--- merge-image-for-cmp.py
+++ merge-image-for-cmp.py
@@ -2,12 +2,24 @@ import subprocess, os, re
 
 print("merge manuform images for comparison")
 
+# thumb key count
 subprocess.run([
     "montage",
     "-geometry",
     "+0",
     "manuform/manuform-4x6+[2-6]-DIAGONAL.png",
     "manuform/manuform-4x6+x-DIAGONAL-cmp.png"])
+
+# center of the curvature for the column
+subprocess.run([
+    "montage",
+    "-geometry",
+    "+0",
+    "-tile",
+    "4x1",
+    "manuform/manuform-4x6+6-(curve.centercol=*)-DIAGONAL.png",
+    "manuform/manuform-4x6+6-DIAGONAL.png",
+    "manuform/manuform-4x6+6-(curve.centercol)-DIAGONAL-cmp.png"])
 
 for file in os.listdir('manuform'):
     match = re.match(r'manuform-(?P<keys>[\dx+]+)-(?P<param>\(.+\))-(?P<camera>\w+)\.png', file)
@@ -16,15 +28,18 @@ for file in os.listdir('manuform'):
         if "mx-snap-in" in file:
             baseImg = file.replace("mx-snap-in", "mx")
         output = file.replace(".png", "-cmp.png")
-        print(f"merge {file} and {baseImg} to {output}")
-        subprocess.run([
-            "montage",
-            "-geometry",
-            "+0",
-            f"manuform/{file}",
-            f"manuform/{baseImg}",
-            f"manuform/{output}"])
+        # generate cmp image except for param curve.centercol
+        if "curve.centercol" not in file:
+            print(f"merge {file} and {baseImg} to {output}")
+            subprocess.run([
+                "montage",
+                "-geometry",
+                "+0",
+                f"manuform/{file}",
+                f"manuform/{baseImg}",
+                f"manuform/{output}"])
 
+        # highlight the difference between mx-snap-in and mx
         if "mx-snap-in" in file:
             subprocess.run([
                 "convert",
@@ -39,6 +54,7 @@ for file in os.listdir('manuform'):
                 f"manuform/{output}",
                 f"manuform/{output}"])
 
+        # highlight the difference between choc and box
         if "choc" in file:
             subprocess.run([
                 "convert",
