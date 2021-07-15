ce721366526e4f6e7b5a0b59159212f35649edbb
Jian Feng Ye <yejianfengblue@163.com>
2021-07-15 10:41:43 +0800

    Cmp mx-snap-in with mx. Highlight the difference of mx-snap-in and choc

--- merge-image-for-cmp.py
+++ merge-image-for-cmp.py
@@ -8,6 +8,8 @@ for file in os.listdir('manuform'):
     match = re.match(r'manuform-(?P<keys>[\dx+]+)-(?P<param>\(.+\))-(?P<camera>\w+)\.png', file)
     if match:
         baseImg = f"manuform-{match.group('keys')}-{match.group('camera')}.png"
+        if "mx-snap-in" in file:
+            baseImg = file.replace("mx-snap-in", "mx")
         output = file.replace(".png", "-cmp.png")
         print(f"merge {file} and {baseImg} to {output}")
         os.system(f"montage -geometry +0+0 manuform/{file} manuform/{baseImg} manuform/{output}".replace("(", "\(").replace(")", "\)"))
